00e6dadf75c05b9a07c8ed165129911e86cb0707
ChrisL233 <111047983+ChrisL233@users.noreply.github.com>
2023-06-06 20:34:43 +0800

    Update setup_ikev2_server.sh

--- setup_ikev2_server.sh
+++ setup_ikev2_server.sh
@@ -1,65 +1,209 @@
 #!/bin/bash
+# This script will set up an iKev2 server on an Ubuntu 20.04 LTS system.
 
-# Update the system
-sudo apt update
-sudo apt upgrade -y
+# Make sure only root can run this script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
-# Install strongSwan
-sudo apt install -y strongswan
+# Get the server's public IP address
+PUBLIC_IP=$(curl -s https://ipinfo.io/ip)
 
-# Configure IP forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl -w net.ipv6.conf.all.forwarding=1
+# Ask the user for the server name and the first client's credentials
+read -p "Enter your server name: " SERVER_NAME
+read -p "Enter your first client's name: " CLIENT_NAME
+read -s -p "Enter your first client's password: " CLIENT_PASSWORD
+echo
 
-# Enable IP forwarding on boot
-sudo sed -i '/^#net.ipv4.ip_forward=1/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
-sudo sed -i '/^#net.ipv6.conf.all.forwarding=1/c\net.ipv6.conf.all.forwarding=1' /etc/sysctl.conf
+# Install the necessary packages
+apt update && apt upgrade -y
+apt install -y strongswan strongswan-pki libcharon-extra-plugins strongswan-pkcs11 openssl
 
-# Generate a strongSwan configuration
-sudo bash -c 'cat > /etc/ipsec.conf << EOL
+# Create the CA certificate
+mkdir -p ~/pki/{cacerts,certs,private}
+chmod 700 ~/pki
+cd ~/pki
+
+ipsec pki --gen --type rsa --size 4096 --outform pem > private/ca.key.pem
+ipsec pki --self --ca --lifetime 3650 --in private/ca.key.pem --type rsa --dn "CN=VPN CA" --outform pem > cacerts/ca.cert.pem
+
+# Create the Server certificate
+ipsec pki --gen --type rsa --size 4096 --outform pem > private/vpn-server.key.pem
+ipsec pki --pub --in private/vpn-server.key.pem --type rsa | ipsec pki --issue --lifetime 1825 --cacert cacerts/ca.cert.pem --cakey private/ca.key.pem --dn "CN=$PUBLIC_IP" --san "$SERVER_NAME" --flag serverAuth --flag ikeIntermediate --outform pem > certs/vpn-server.cert.pem
+
+# Create the Client certificate
+ipsec pki --gen --type rsa --size 2048 --outform pem > private/vpn-client.key.pem
+ipsec pki --pub --in private/vpn-client.key.pem --type rsa | ipsec pki --issue --lifetime 1825 --cacert cacerts/ca.cert.pem --cakey private/ca.key.pem --dn "CN=$CLIENT_NAME" --outform pem > certs/vpn-client.cert.pem
+
+# Move the keys to /etc/ipsec.d/
+cp -r ~/pki/* /etc/ipsec.d/
+
+# Configure ipsec
+echo "
 config setup
-  uniqueids=never
-  charondebug="cfg 2, dmn 2, ike 2, net 2"
+    charondebug=\"ike 1, knl 1, cfg 0\"
+    uniqueids=no
 
 conn ikev2-vpn
-  auto=add
-  compress=no
-  type=tunnel
-  keyexchange=ikev2
-  fragmentation=yes
-  forceencaps=yes
-  ike=aes256-sha256-modp2048!
-  esp=aes256-sha256!
-  dpdaction=clear
-  dpddelay=300s
-  rekey=no
-  left=%any
-  leftid=%any
-  leftcert=server-cert.pem
-  leftsendcert=always
-  leftsubnet=0.0.0.0/0
-  right=%any
-  rightid=%any
-  rightauth=eap-mschapv2
-  rightsourceip=10.10.10.0/24
-  rightsendcert=never
-  eap_identity=%identity
-EOL'
+    auto=add
+    compress=no
+    type=tunnel
+    keyexchange=ikev2
+    fragmentation=yes
+    forceencaps=yes
+    ike=aes256-sha1-modp1024,3des-sha1-modp1024!
+    esp=aes256-sha1,3des-sha1!
+    dpdaction=clear
+    dpddelay=300s
+    rekey=no
+    left=%any
+    leftid=@$SERVER_NAME
+    leftcert=/etc/ipsec.d/certs/vpn-server.cert.pem
+    leftsendcert=always
+    leftsubnet=0.0.0.0/0
+    right=%any
+    rightid=%any
+    rightauth=eap-mschapv2
+    rightsourceip=10.10.10.0/24
+    rightdns=8.8.8.8,8.8.4.4
+    rightsendcert=never
+    eap_identity=%identity
+" > /etc/ipsec.conf
 
-# Generate a strongSwan secrets file
-read -p "Enter VPN username: " vpn_username
-read -s -p "Enter VPN password: " vpn_password
+# Configure strongswan
+echo "
+charon {
+    load_modular = yes
+    duplicheck.enable = no
+    compress = yes
+    plugins {
+        include strongswan.d/charon/*.conf
+    }
+}
+include strongswan.d/*.conf
+" > /etc/strongswan.conf
 
-sudo bash -c "echo -n \"$vpn_username : EAP : \"$vpn_password > /etc/ipsec.secrets"
+# Set the VPN credentials
+echo "$CLIENT_NAME : EAP \"$CLIENT_PASSWORD\"" > /etc/ipsec.secrets
 
-# Generate a self-signed server certificate
-sudo ipsec pki --gen --type rsa --size 4096 --outform pem > server-key.pem
-sudo ipsec pki --self --ca --lifetime 3650 --in server-key.pem --type rsa --dn "CN=VPN Server" --outform pem > server-cert.pem
+# Restart IPsec service
+systemctl restart strongswan-starter
 
-# Update permissions for the private key
-sudo chmod 600 /etc/ipsec.secrets /etc/ipsec.d/private/server-key.pem
+# Enable IPsec service at startup
+systemctl enable strongswan-starter
 
-# Restart strongSwan service
-sudo systemctl restart strongswan
+# Enable IP forwarding
+echo "net.ipv4.ip_forward = 1" > /etc/sysctl.d/99-sysctl.conf
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.d/99-sysctl.conf
+sysctl -p
 
-echo "IKEv2 VPN server is now set up."
+# Update the firewall rules
+ufw allow OpenSSH
+ufw enable
+ufw allow 500,4500/udp
+
+# Export client certificate and private key to p12
+openssl pkcs12 -export -out /etc/ipsec.d/p12/$CLIENT_NAME.p12 -inkey /etc/ipsec.d/private/vpn-client.key.pem -in /etc/ipsec.d/certs/vpn-client.cert.pem -certfile /etc/ipsec.d/cacerts/ca.cert.pem -passout pass:strongSwan
+
+# Generate mobileconfig file for iOS
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>PayloadContent</key>
+    <array>
+        <dict>
+            <key>IKEv2</key>
+            <dict>
+                <key>AuthName</key>
+                <string>$CLIENT_NAME</string>
+                <key>AuthPassword</key>
+                <string>$CLIENT_PASSWORD</string>
+                <key>AuthenticationMethod</key>
+                <string>None</string>
+                <key>ChildSecurityAssociationParameters</key>
+                <dict>
+                    <key>DiffieHellmanGroup</key>
+                    <integer>2</integer>
+                    <key>EncryptionAlgorithm</key>
+                    <string>AES-256</string>
+                    <key>IntegrityAlgorithm</key>
+                    <string>SHA2-256</string>
+                </dict>
+                <key>DeadPeerDetectionRate</key>
+                <string>Medium</string>
+                <key>DisableMOBIKE</key>
+                <integer>0</integer>
+                <key>DisableRedirect</key>
+                <integer>0</integer>
+                <key>EnableCertificateRevocationCheck</key>
+                <integer>0</integer>
+                <key>EnablePFS</key>
+                <integer>0</integer>
+                <key>IKESecurityAssociationParameters</key>
+                <dict>
+                    <key>DiffieHellmanGroup</key>
+                    <integer>2</integer>
+                    <key>EncryptionAlgorithm</key>
+                    <string>AES-256</string>
+                    <key>IntegrityAlgorithm</key>
+                    <string>SHA2-256</string>
+                    <key>LifeTimeInMinutes</key>
+                    <integer>1440</integer>
+                </dict>
+                <key>LocalIdentifier</key>
+                <string>$CLIENT_NAME</string>
+                <key>PayloadCertificateUUID</key>
+                <string>$SERVER_NAME</string>
+                <key>RemoteAddress</key>
+                <string>$PUBLIC_IP</string>
+                <key>RemoteIdentifier</key>
+                <string>$SERVER_NAME</string>
+                <key>UseConfigurationAttributeInternalIPSubnet</key>
+                <integer>0</integer>
+                <key>UserDefinedName</key>
+                <string>$SERVER_NAME</string>
+            </dict>
+            <key>IPv4</key>
+            <dict>
+                <key>OverridePrimary</key>
+                <integer>1</integer>
+            </dict>
+            <key>PayloadDescription</key>
+            <string>Adds VPN settings</string>
+            <key>PayloadDisplayName</key>
+            <string>VPN</string>
+            <key>PayloadIdentifier</key>
+            <string>com.apple.vpn.managed.$CLIENT_NAME</string>
+            <key>PayloadOrganization</key>
+            <string>$SERVER_NAME</string>
+            <key>PayloadType</key>
+            <string>com.apple.vpn.managed</string>
+            <key>PayloadUUID</key>
+            <string>$SERVER_NAME</string>
+            <key>PayloadVersion</key>
+            <integer>1</integer>
+            <key>Proxies</key>
+            <dict/>
+        </dict>
+    </array>
+    <key>PayloadDisplayName</key>
+    <string>$SERVER_NAME</string>
+    <key>PayloadIdentifier</key>
+    <string>com.$SERVER_NAME.vpn.managed</string>
+    <key>PayloadRemovalDisallowed</key>
+    <false/>
+    <key>PayloadType</key>
+    <string>Configuration</string>
+    <key>PayloadUUID</key>
+    <string>$SERVER_NAME</string>
+    <key>PayloadVersion</key>
+    <integer>1</integer>
+</dict>
+</plist>" > /etc/ipsec.d/mobileconfig/$CLIENT_NAME.mobileconfig
+
+# Print the location of the files
+echo "The .p12 file for Android is available at /etc/ipsec.d/p12/$CLIENT_NAME.p12"
+echo "The .mobileconfig file for iOS is available at /etc/ipsec.d/mobileconfig/$CLIENT_NAME.mobileconfig"
+
